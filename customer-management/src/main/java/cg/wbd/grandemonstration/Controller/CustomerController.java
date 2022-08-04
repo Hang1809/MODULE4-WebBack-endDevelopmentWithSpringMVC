df9909050c330f22a2a05c430039d093ea53f3d6
Hang1809 <hangdinhphankhanh@gmail.com>
2022-08-04 07:58:08 +0700

    update

--- customer-management/src/main/java/cg/wbd/grandemonstration/Controller/CustomerController.java
+++ customer-management/src/main/java/cg/wbd/grandemonstration/Controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +16,8 @@ import java.util.List;
 
 @Controller
 public class CustomerController {
+
+
     @Autowired
     private CustomerService customerService;
 
@@ -24,4 +28,21 @@ public class CustomerController {
         modelAndView.addObject("customers", customers);
         return modelAndView;
     }
+
+    @PostMapping("/customers")
+    public ModelAndView showList(HttpServletRequest req){
+        ModelAndView modelAndView = new ModelAndView("customers/list.jsp");
+        List<Customer> customers = customerService.findAll();
+        Customer customer = new Customer();
+        customer.setName(req.getParameter("name"));
+        customer.setEmail(req.getParameter("email"));
+        customer.setAddress(req.getParameter("address"));
+        Long id = Long.valueOf(req.getParameter("id"));
+        customer.setId(id);
+        customerService.save(customer);
+        modelAndView.addObject("customers", customers);
+        return  modelAndView;
+
+    }
+    
 }
